8f97b9b29be6b3f60b0db036f85910282c84e238
Samantha Covington <samanthanc_08@yahoo.com>
2019-10-22 21:11:18 -0500

    adding files

---
+++ bamazonCustomer.js
@@ -0,0 +1,69 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+// clears table and displays all products to user
+function getAllProducts() {
+    connection.query("SELECT * FROM products", function(err, data) {
+        console.log(data);
+        promptUser();
+    });
+}
+
+// prompt user for prechase- item # and item qty
+function promptUser() {
+    inquirer.prompt([{
+            name: "name",
+            type: "input",
+            message: "Please enter the item you would like to purhcase.",
+            filter: Number
+        },
+        {
+            name: "Quantity",
+            type: "input",
+            message: "How many items do you wish to purchase?",
+            filter: Number
+        },
+
+    ]).then(function(answers) {
+        var quantityNeeded = answers.Quantity;
+        var IDrequested = answers.ID;
+        purchaseOrder(IDrequested, quantityNeeded);
+    });
+    // console.log("prompting user");
+};
+
+
+
+
+function updateItemById(updateItem, customerOrder) {
+    // var query = "UPDATE products SET stock_quantity = stock_quantity-" + customerOrder + " WHERE item_id =" + updateItem;
+    // console.log(query);
+    connection.query("UPDATE products SET stock_quantity = stock_quantity-" + customerOrder + " WHERE item_id =" + updateItem, function(err, data) {
+        console.log(data);
+    });
+}
+
+function getProductById(productID) {
+    connection.query("SELECT * FROM products WHERE item_id =" + productID, function(err, data) {
+        console.log(data);
+    });
+}
+
+getAllProducts();
---
+++ seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("21' flatscreen TV", "Electronics", 100.00, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("26' flatscreen TV","Electronics",150.25, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("V-Neck T-shirt", "Womens", 19.98, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("skinny leg jeans","Womens",25.95, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("button-up Polo", "Mens",19.98, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Bootcut jean","Mens",30.95, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Brown Hair Dye", "Cosmetics", 22.78, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("False eyelashes","Cosmetics",10.32, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Blanket", "Baby", 35.54, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Brown Crib","Baby",255.48, 5);
+
+
+
